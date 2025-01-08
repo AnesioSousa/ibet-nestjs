dd12d0de487f4a123afbc4503e7c949dea2169f2
Your Name <anesios98@gmail.com>
2025-01-08 19:56:51 -0300

    :star: setup database

---
+++ src/config/config.service.ts
@@ -0,0 +1,56 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+require('dotenv').config();
+
+class ConfigService {
+    constructor(private env: { [k: string]: string | undefined }) { }
+
+    private getValue(key: string, throwOnMissing = true): string {
+        const value = this.env[key];
+        if (!value && throwOnMissing) {
+            throw new Error(`config error - missing env.${key}`);
+        }
+        return value;
+    }
+
+    public ensureValues(keys: string[]) {
+        keys.forEach(k => this.getValue(k, true));
+        return this;
+    }
+
+    public getPort() {
+        return this.getValue('PORT', true);
+    }
+
+    public isProduction() {
+        const node = this.getValue('MODE', false);
+        return node != 'DEV';
+    }
+
+    public getTypeOrmConfig(): TypeOrmModuleOptions {
+        return {
+            type: 'postgres',
+            host: this.getValue('POSTGRES_HOST'),
+            port: parseInt(this.getValue('POSTGRES_PORT')),
+            username: this.getValue('POSTGRES_USER'),
+            password: this.getValue('POSTGRES_PASSWORD'),
+            database: this.getValue('POSTGRES_DATABASE'),
+            entities: ['**/*.entity{.ts,.js}'],
+            migrationsTableName: 'migration',
+            migrations: ['dist/src/migration/*.js'], // Note que os arquivos precisam estar no dist se estiver em produção
+            synchronize: false, // Evite usar "synchronize: true" em produção
+            ssl: this.isProduction(),
+        };
+    }
+}
+
+const configService = new ConfigService(process.env)
+    .ensureValues([
+        'POSTGRES_HOST',
+        'POSTGRES_PORT',
+        'POSTGRES_USER',
+        'POSTGRES_PASSWORD',
+        'POSTGRES_DATABASE'
+    ]);
+
+export { configService };
